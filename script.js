a38a55d0ca9a85b39711282df8cea8d5d1ed2026
KaramReqmany <karamreqmany546@gmail.com>
2024-10-09 16:37:04 +0300

    up

---
+++ script.js
@@ -0,0 +1,11 @@
+document.write('karam');
+var firstDiv=document.querySelector('.first-div');
+firstDiv.innerHTML= "hello from focalx";
+firstDiv.style.backgroundColor='red';
+firstDiv.style.height='300px';
+console.log(firstDiv);
+function sum(x,y){
+    console.log(x+y);
+}
+for(let i=0;i<Array.length;i++)
+let x=10;
